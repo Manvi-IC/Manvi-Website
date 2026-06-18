48b968001be5aa0addb3247ffa1b33506f5baec3
Altafraza07 <altafrzakgn@gmail.com>
2026-06-18 13:25:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*',
+        destination: `${API_URL}/:path*`,
       },
     ];
   },
